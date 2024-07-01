48d6ddf1bd9ef331c9cb9d15bcc9f865d3b04ad1
afruzbekh <afruzbek1595@gmail.com>
2024-07-01 23:01:11 +0500

    inital commit

---
+++ src/Ant-Design/AntDesign.jsx
@@ -0,0 +1,1 @@
+import { Rate } from "antd";
--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import colorPicker from "./Ant-Design/AntDesign.jsx";
+import { Button } from "antd";
+import { Rate } from "antd";
+import { Image } from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <colorPicker /> */}
+      <div className="container">
+        <div className="flex-box-cards">
+          <div className="card">
+            <div className="card__image-container">
+              <Image
+                className="card__image"
+                src="https://images.unsplash.com/photo-1487015307662-6ce6210680f1?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="img"
+              />
+            </div>
+            <Rate allowHalf defaultValue={2.5} />
+          </div>
+        </div>
+      </div>
+
+      {/* <Button type="primary">Only Clickable Button</Button> */}
     </div>
   );
 }
