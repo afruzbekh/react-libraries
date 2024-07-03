f42fbc849cb47f5b1480e7dd429dbe7112915763
afruzbekh <afruzbek1595@gmail.com>
2024-07-03 22:11:33 +0500

    fetched

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Rate } from "antd";
 import { Image } from "antd";
 
+const titleLimit = (title, wordLimit) => {
+  const words = title.split(" ");
+  if (words.length > wordLimit) {
+    return words.slice(0, wordLimit).join(" ") + "...";
+  }
+  return title;
+};
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="flex-box-cards">
-          <div className="card">
-            <div className="card__image-container">
-              <Image
-                className="card__image"
-                src="https://images.unsplash.com/photo-1487015307662-6ce6210680f1?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="img"
-              />
+          {products.map((product) => (
+            <div key={product.id} className="card">
+              <div className="card__image-container">
+                <Image
+                  className="card__image"
+                  src={product.image}
+                  alt={product.title}
+                />
+              </div>
+              <span className="product-name">{titleLimit(product.title, 5)}</span>
+              <Rate allowHalf defaultValue={product.rating.rate} />
+              <span className="product-price">${product.price}</span>
             </div>
-            <span className="product-name">Square Clear Glass Box</span>
-            <Rate allowHalf defaultValue={2.5} />
-            <span className="product-price">$258</span>
-          </div>
+          ))}
         </div>
       </div>
     </div>
